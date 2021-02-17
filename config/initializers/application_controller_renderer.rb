ea4e5052c6a375e261d67c91d64f282c7c51801d
Nakajima3106 <uver777.333@gmail.com>
2021-02-17 01:15:07 +0000

    3章終了

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'd34c29da7516f1b30fb25fd7f3ea68b17ae7931e9846113666cb80f52036c004b420588f96d5e94690d356178665ffeb7e3fa33fd7489a4dbba5d75ada96d197'
